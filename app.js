d2cbb1be353f7dec85e76cd1086ebf2b25ae0a85
Mujeeb932 <78494720+Mujeeb932@users.noreply.github.com>
2021-03-31 07:17:32 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var num = prompt ("enter marks");
+if(num == 400)
+{
+    alert("Grade A");
+}
+else if (num == 300)
+{
+    alert("Grade B");
+
+}
+else if(num == 200)
+{
+    alert("Grade C");
+}
+else if(num == 150)
+{
+    alert("Grade D");
+}
+else if(num == 100)
+{
+    alert("fail")
+}
+else {
+  alert  ("TRY AGAIN LETTER")
+}
